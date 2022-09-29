d58a74634f1a2506a93f58b9e9ed62f721638bba
Tunku S <tunkuas@gmail.com>
2022-09-29 10:02:56 +0700

    Update sgb.php

--- sgb.php
+++ sgb.php
@@ -2,3 +2,24 @@
 
 $sgb = "sgb";
 echo $sgb;
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+$y = 10;
+
+function myTest() {
+  $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
+} 
+
+myTest();
+echo $y;
+?>
+
+<?= $sgb; ?>
+</body>
+</html>
