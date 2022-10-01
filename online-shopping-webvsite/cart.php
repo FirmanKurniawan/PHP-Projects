8ea5ad2152bcd96599d147cd01a519a79b0bb0ba
ashutosh7i <ashutoshsoni130@gmail.com>
2022-10-01 18:02:42 +0530

    Added a Online Shopping website Project
    contribution from @ashutosh7i for hacktoberfest 2022

---
+++ online-shopping-webvsite/cart.php
@@ -0,0 +1,82 @@
+<?php
+    session_start();
+    require 'connection.php';
+    if(!isset($_SESSION['email'])){
+        header('location: login.php');
+    }
+    $user_id=$_SESSION['id'];
+    $user_products_query="select it.id,it.name,it.price from users_items ut inner join items it on it.id=ut.item_id where ut.user_id='$user_id'";
+    $user_products_result=mysqli_query($con,$user_products_query) or die(mysqli_error($con));
+    $no_of_user_products= mysqli_num_rows($user_products_result);
+    $sum=0;
+    if($no_of_user_products==0){
+        //echo "Add items to cart first.";
+    ?>
+        <script>
+        window.alert("No items in the cart!!");
+        </script>
+    <?php
+    }else{
+        while($row=mysqli_fetch_array($user_products_result)){
+            $sum=$sum+$row['price']; 
+       }
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Projectworlds Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php 
+               require 'header.php';
+            ?>
+            <br>
+            <div class="container">
+                <table class="table table-bordered table-striped">
+                    <tbody>
+                        <tr>
+                            <th>Item Number</th><th>Item Name</th><th>Price</th><th></th>
+                        </tr>
+                       <?php 
+                        $user_products_result=mysqli_query($con,$user_products_query) or die(mysqli_error($con));
+                        $no_of_user_products= mysqli_num_rows($user_products_result);
+                        $counter=1;
+                       while($row=mysqli_fetch_array($user_products_result)){
+                           
+                         ?>
+                        <tr>
+                            <th><?php echo $counter ?></th><th><?php echo $row['name']?></th><th><?php echo $row['price']?></th>
+                            <th><a href='cart_remove.php?id=<?php echo $row['id'] ?>'>Remove</a></th>
+                        </tr>
+                       <?php $counter=$counter+1;}?>
+                        <tr>
+                            <th></th><th>Total</th><th>Rs <?php echo $sum;?>/-</th><th><a href="success.php?id=<?php echo $user_id?>" class="btn btn-primary">Confirm Order</a></th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <br><br><br><br><br><br><br><br><br><br>
+            <footer class="footer">
+               <div class="container">
+                <center>
+                   <p>Copyright &copy <a href="https://projectworlds.in">Projectworlds</a> Store. All Rights Reserved.</p>
+                   <p>This website is developed by Yugesh Verma</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
---
+++ online-shopping-webvsite/login.php
@@ -0,0 +1,64 @@
+<?php
+    require 'connection.php';
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Projectworlds Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <br><br><br>
+           <div class="container">
+                <div class="row">
+                    <div class="col-xs-6 col-xs-offset-3">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <h3>LOGIN</h3>
+                            </div>
+                            <div class="panel-body">
+                                <p>Login to make a purchase.</p>
+                                <form method="post" action="login_submit.php">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" name="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" name="password" placeholder="Password(min. 6 characters)" pattern=".{6,}">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" value="Login" class="btn btn-primary">
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="panel-footer">Don't have an account yet? <a href="signup.php">Register</a></div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+           <br><br><br><br><br>
+           <footer class="footer">
+               <div class="container">
+                <center>
+                   <p>Copyright &copy <a href="https://projectworlds.in">Projectworlds</a> Store. All Rights Reserved.</p>
+                   <p>This website is developed by Yugesh Verma</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
